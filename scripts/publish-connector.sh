8c72468a3557a2eac96315c238d434a8991eceb7
Taotao Wang <135496506+wangtaotao0524@users.noreply.github.com>
2025-05-29 09:44:15 +0800

    Update publish-connector.sh

--- scripts/publish-connector.sh
+++ scripts/publish-connector.sh
@@ -24,16 +24,16 @@ latest_tag="$image:latest"
 version_tag="$image:$connector_version"
 echo "Image version tag: $version_tag"
 
-docker manifest inspect $version_tag > /dev/null
-if [ "$?" == 1 ]; then
-  docker build . \
-    --build-arg path=$connector_path \
-    --build-arg version=$connector_version \
-    --pull \
-    -t $latest_tag \
-    -t $version_tag \
-    --label "io.airbyte.version=$connector_version" \
-    --label "io.airbyte.name=$image"
-  docker push $latest_tag
-  docker push $version_tag
-fi
+# docker manifest inspect $version_tag > /dev/null
+# if [ "$?" == 1 ]; then
+#   docker build . \
+#     --build-arg path=$connector_path \
+#     --build-arg version=$connector_version \
+#     --pull \
+#     -t $latest_tag \
+#     -t $version_tag \
+#     --label "io.airbyte.version=$connector_version" \
+#     --label "io.airbyte.name=$image"
+#   docker push $latest_tag
+#   docker push $version_tag
+# fi
